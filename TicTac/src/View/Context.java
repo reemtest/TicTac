6ea28c609ea0196e4ca8f4ba7664e2e36dbf2b30
reem <reemmohamed9966@gmail.com>
2024-01-19 05:43:09 +0200

    adding features

---
+++ TicTac/src/View/Context.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package View;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Context {
+    private State state;
+
+    public State getState() {
+        return state;
+    }
+
+    public void setState(State state) {
+        this.state = state;
+    }
+    
+    
+    
+}
---
+++ TicTac/src/View/OState.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package View;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class OState implements State {
+    public void Symbolizing(Context context)
+    {
+    
+    
+    }
+}
