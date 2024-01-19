ee06153a2b3490c3439403ac4df2aea55ac75f19
reem <reemmohamed9966@gmail.com>
2024-01-19 04:47:45 +0200

    Changed GUI

--- TicTac/src/Model/Model.java
+++ TicTac/src/Model/Model.java
@@ -2,12 +2,44 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package tictac;
+package Model;
+
+import java.util.Scanner;
 
 /**
  *
  * @author 20115
  */
 public class Model {
+    
+    
+  
+    public void playTwoPlayers()
+    {   
+         
+        
+    }
+    
+    public void playWithComputer()
+    {
+    
+    
+    
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
 }
---
+++ TicTac/src/Model/O.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class O {
+     private String  shape;
+
+    public String getShape() {
+        return shape;
+    }
+     
+     
+     
+     
+     
+     
+}
