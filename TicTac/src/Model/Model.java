8ef2ef6e6f6fb637f855149f2fa46f95ccde021a
Hagar Adel <115931896+hagar03@users.noreply.github.com>
2024-01-24 02:40:56 +0200

    Check for winning cases

--- TicTac/src/Model/Model.java
+++ TicTac/src/Model/Model.java
@@ -11,7 +11,8 @@ import java.util.Scanner;
  * @author 20115
  */
 public class Model {
-    
+        private static Model instance;
+
 private char [][] Board;
 
 public Model()
@@ -28,14 +29,53 @@ public Model()
             }
         }
     }
+   
+       public static synchronized Model getInstance() {
+        if (instance == null) {
+            instance = new Model();
+        }
+        return instance;
+    }
+       
+       public char [][] getBoard()
+       {
+           return Board;
+       }
+
+    public void playTwoPlayers(int row, int col, char symbol) {
+        // Check if the cell is empty before making a move
+        if (Board[row][col] == ' ') {
+            Board[row][col] = symbol;
+        }
+    }
   
-    public void playTwoPlayers()
-    {   
-         
-        
+    public char checkForWinner() {
+        // Check rows
+        for (int i = 0; i < 3; i++) {
+            if (Board[i][0] == Board[i][1] && Board[i][1] == Board[i][2] && Board[i][0] != ' ') {
+                return Board[i][0]; // Winner found in row
+            }
+        }
+
+        // Check columns
+        for (int j = 0; j < 3; j++) {
+            if (Board[0][j] == Board[1][j] && Board[1][j] == Board[2][j] && Board[0][j] != ' ') {
+                return Board[0][j]; // Winner found in column
+            }
+        }
+
+        // Check diagonals
+        if (Board[0][0] == Board[1][1] && Board[1][1] == Board[2][2] && Board[0][0] != ' ') {
+            return Board[0][0]; // Winner found in diagonal (top-left to bottom-right)
+        }
+        if (Board[0][2] == Board[1][1] && Board[1][1] == Board[2][0] && Board[0][2] != ' ') {
+            return Board[0][2]; // Winner found in diagonal (top-right to bottom-left)
+        }
+
+        return ' '; // No winner yet
     }
     
-   /* public void playWithComputer()
+  /*  public void playWithComputer()
     {
     
     
@@ -44,17 +84,4 @@ public Model()
     */
     
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-}
+    }
